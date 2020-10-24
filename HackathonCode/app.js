13ceead736721b5638d655018f20654abf3356e4
Grady Whelan <32005439+gradyw@users.noreply.github.com>
2020-10-24 14:41:11 -0700

    homepage works except for images

--- HackathonCode/app.js
+++ HackathonCode/app.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 var express = require('express');
 var ejs = require('ejs');
 const { runInNewContext } = require('vm');
+const { url } = require('inspector');
 var app = express();
 
 const views = __dirname + '/views/';
@@ -14,6 +15,11 @@ app.get('/', (req, res) => {
     })
 })
 
+
+app.post('/', urlencodedParser, (req, res) => {
+    console.log(req);
+    res.redirect('/'+ req.body.name);
+})
 
 // SAMPLE BELOW, copy for your own pages
 // app.get('/input', (req, res) => {
